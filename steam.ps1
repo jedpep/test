8e01bc0a4dd44219f725ef8e368a489715f24d87
jedpep <90096413+jedpep@users.noreply.github.com>
2024-09-26 18:35:16 +0100

    Update steam.ps1

--- steam.ps1
+++ steam.ps1
@@ -1,6 +1,7 @@
-$installDir = "C:\Users\$Env:UserName\steam"
+$installDir = "C:\Users\$Env:UserName\Steam"
 New-Item -ItemType Directory -Path $installDir -Force
 
-Invoke-RestMethod "https://cdn.akamai.steamstatic.com/client/installer/SteamSetup.exe" -OutFile "steam.exe"
+Invoke-RestMethod "https://cdn.akamai.steamstatic.com/client/installer/SteamSetup.exe" -OutFile "Steam.exe"
 
-cmd.exe /c "set __COMPAT_LAYER=RUNASINVOKER && start steam.exe --install-path=$installDir"
+cmd.exe /c "set __COMPAT_LAYER=RUNASINVOKER && start Steam /S /D=$installDir"
+cmd.exe /c "$installDir\Steam\Steam"
